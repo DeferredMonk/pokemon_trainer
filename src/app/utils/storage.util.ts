22a68e00a85a0978f9878e031ed0e2d7c5d91c49
DeferredMonk <marcoangeli151@gmail.com>
2023-02-06 21:13:11 +0200

    docs: added documentation to catalogue page sub components and services

--- src/app/utils/storage.util.ts
+++ src/app/utils/storage.util.ts
@@ -1,25 +1,23 @@
 export class StorageUtil {
-    
-    public static storageSave<T>(key: string, value:any): void{
-        sessionStorage.setItem(key, JSON.stringify(value));
-    }
+  public static storageSave<T>(key: string, value: any): void {
+    sessionStorage.setItem(key, JSON.stringify(value));
+  }
 
-    public static storageRead<T>(key: string): T | undefined{
-        const storedValue = sessionStorage.getItem(key);
-        try{
-            if(storedValue){
-                return JSON.parse(storedValue) as T;
-            }
-            
-            return undefined;
-            
-        }catch (e){
-            sessionStorage.removeItem(key);
-            return undefined;
-        }
-    }
+  public static storageRead<T>(key: string): T | undefined {
+    const storedValue = sessionStorage.getItem(key);
+    try {
+      if (storedValue) {
+        return JSON.parse(storedValue) as T;
+      }
 
-    public static storageDelete<T>(key: string): void {
-        sessionStorage.removeItem(key);
+      return undefined;
+    } catch (e) {
+      sessionStorage.removeItem(key);
+      return undefined;
     }
+  }
+
+  public static storageDelete<T>(key: string): void {
+    sessionStorage.removeItem(key);
+  }
 }
